664e421f9a54a2a5176e6247d61c8b32afa06847
fadezilla <harleyseierstad@hotmail.com>
2023-12-06 18:01:25 +0100

    indexForCustomerAndProduct

--- customer/app.js
+++ customer/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
 var customerRouter = require('./routes/customer');
 var app = express();
 
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/', customerRouter);
 
 // catch 404 and forward to error handler
--- products/app.js
+++ products/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
 var productRouter = require('./routes/product');
 var app = express();
 
@@ -17,7 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/', productRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
